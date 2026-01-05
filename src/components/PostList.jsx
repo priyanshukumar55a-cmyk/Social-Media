448bab6b6bb50d6f8c262451a13f94f18a248c86
Priyanshu Kumar <priyanshukumar55a@gmail.com>
2026-01-05 22:29:50 +0530

    Fix Loading of data and its effects

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,20 +2,39 @@ import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import { useEffect } from "react";
+import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList=()=>{
   const {postList,addInitialPosts} = useContext(PostListData)
+  const [fetching,setFetching] = useState(false);
 
-  const handleGetPostsClick=()=>{
-    fetch('https://dummyjson.com/posts')
+  useEffect(()=>{
+    if (postList.length > 0) return;
+
+    const controller=new AbortController();
+    const signal=controller.signal;
+
+    setFetching(true);
+    fetch('https://dummyjson.com/posts' , {signal})
     .then(res => res.json())
-    .then(data=>addInitialPosts(data.posts));
-  }
+    .then(data=>{
+      addInitialPosts(data.posts);
+      setFetching(false);
+    });
+
+    return()=>{
+      console.log("cleaning up UseEffect.")
+      controller.abort();
+    }
+  },[])
 
   return(
     <>
-    {postList.length===0 && <WelcomeMessage onGetPostsClick={handleGetPostsClick}/>}
-    {postList.map(post=><Post key={post.id} post={post}/>)}
+    {fetching && <LoadingSpinner/>}
+    {!fetching && !<LoadingSpinner/> && postList.length===0 && <WelcomeMessage />}
+    {!fetching && postList.map(post=><Post key={post.id} post={post}/>)}
     </>
   )
 }
